651ff21202563c880812c1cf6eea1711c786f1a5
Jv_Rdgz <javier.rodriguezjr97@gmail.com>
2020-02-24 13:57:23 +0100

    New projects added, and some files upgraded

---
+++ Programacion_1DAM/Workspace Programacion Primero/Swing/src/main/Main.java
@@ -0,0 +1,20 @@
+package main;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author javie
+ */
+public class Main {
+    public static void main(String []args) {
+        Interfaz i = new Interfaz();
+        
+        i.setTitle("Actividad 7");
+        i.setVisible(true);
+    }
+}

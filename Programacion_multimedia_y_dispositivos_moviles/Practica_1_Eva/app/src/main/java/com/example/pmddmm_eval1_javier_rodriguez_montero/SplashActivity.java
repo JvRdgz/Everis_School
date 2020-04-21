0a29f225ccc92f9cfbeb9141fc0e6d09911d6f3b
Jv_Rdgz <javier.rodriguezjr97@gmail.com>
2020-04-21 03:29:47 +0200

    EMPEZANDO PRÁCTICA 1 EVA PROGRAMACIÓN DE DISPOSITIVOS MÓVILES.
    Dentro de la carpeta Practica Eva 1, hay otra carpeta llamada Documentación que
    contiene todo los pasos seguidos en el desarrollo del proyecto, junto con
    screenshots.
    
    Hasta ahora simplemente he creado y terminado el SplashScreen. Tambien he añadido
    los archivos de configuracion para la internacionalización de la aplicación en
    varios idiomas y regiones. Para mas información, consultar el documento adjunto.

---
+++ Programacion_multimedia_y_dispositivos_moviles/Practica_1_Eva/app/src/main/java/com/example/pmddmm_eval1_javier_rodriguez_montero/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.example.pmddmm_eval1_javier_rodriguez_montero;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}

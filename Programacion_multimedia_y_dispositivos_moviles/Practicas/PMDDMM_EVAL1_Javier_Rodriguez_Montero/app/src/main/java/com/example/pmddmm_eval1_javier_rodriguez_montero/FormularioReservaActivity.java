862a9f798176d643276d22f0a7f562bff80d8a9f
Jv_Rdgz <javier.rodriguezjr97@gmail.com>
2020-05-01 04:26:02 +0200

    Terminado Formulario de Reserva.
    He creado el formulario y todas las opciones del mismo. Quiero meter
    una opcion de scroll por la pantalla del formulario. Tengo que ver como
    puedo hacer para que los Toast del menu overflow puedan aparecer segun el
    idioma configurado en el dispositivo. Tambien quiero personalizar el Toolbar
    dado que viene en un azul oscuro que no pega con el color clarito.
    
    He creado un archivo spinner_item_opciones.xml para personalizar el spinner.
    Creo que lo mismo que he hecho con el spinner puedo hacerlo con cualquier otro
    componente de cualquier xml.

---
+++ Programacion_multimedia_y_dispositivos_moviles/Practicas/PMDDMM_EVAL1_Javier_Rodriguez_Montero/app/src/main/java/com/example/pmddmm_eval1_javier_rodriguez_montero/FormularioReservaActivity.java
@@ -0,0 +1,54 @@
+package com.example.pmddmm_eval1_javier_rodriguez_montero;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class FormularioReservaActivity extends AppCompatActivity {
+
+    private Spinner spinner1;
+    // private EditText etNombre, etApellidos, etTelefono, etEmail, etCajaComentario;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_reserva);
+
+        Toast.makeText(this, "Metodo OnCreate del FormularioReservaActivity", Toast.LENGTH_LONG).show();
+
+        /*
+        DE MOMENTO NO SON NECESARIOS
+        etNombre = (EditText)findViewById(R.id.etNombre);
+        etApellidos = (EditText)findViewById(R.id.etApellidos);
+        etTelefono = (EditText)findViewById(R.id.etTelefono);
+        etEmail = (EditText)findViewById(R.id.etEmail);
+        etCajaComentario = (EditText)findViewById(R.id.etCajaComentarios);
+         */
+        spinner1 = (Spinner)findViewById(R.id.spinnerModalidad);
+
+        String[] opciones = {"presencial", "online"};
+
+        ArrayAdapter <String> arrAdapter = new ArrayAdapter<String>(this, R.layout.spinner_item_opciones, opciones); // COMUNICACION DEL ARRAY CON EL SPINNER
+        spinner1.setAdapter(arrAdapter);
+    }
+
+    public void confirmar(View view) {
+        Toast.makeText(this, "Reserva realizada con éxito!", Toast.LENGTH_LONG).show();
+    }
+ /*
+    public boolean esTelefonoValido(String telefono) {
+        return Patterns.PHONE.matcher(telefono).matches();
+    }
+
+    public boolean esCorreoValido(String correo) {
+        return Patterns.EMAIL_ADDRESS.matcher(correo).matches();
+    }
+ */
+}

9a9fe2b57124681e397d424ec00fbdc7df22735c
JvRdgz <javier.rodriguezjr97@gmail.com>
2020-06-03 12:55:29 +0200

    Arreglando repo

--- Programacion_de_servicios_y_procesos/Unidad_1/Actividades/Actividad 7/MostrarMayusculas.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-
-public class MostrarMayusculas {
-    
-    public static void main(String []args) {
-        System.out.println("\n" + args[0].toUpperCase());
-    }
-}
--- README.md
+++
@@ -1,6 +0,0 @@
-# Everis School
-
-Grado superior en Desarrollo de Aplicaciónes Multiplataforma.
-
-En este repositorio iré subiendo todo los ejercicios y prácticas que
-vaya haciendo del temario.

c846bf57b87285fcf91916b6f1f14eab2c6fd389
Jv_Rdgz <javier.rodriguezjr97@gmail.com>
2020-06-04 16:16:39 +0200

    Cosas que faltan por pushear

---
+++ Programacion_multimedia_y_dispositivos_moviles/Practicas/PMDDMM_EVAL1_Javier_Rodriguez_Montero/app/build/generated/source/buildConfig/debug/com/example/pmddmm_eval1_javier_rodriguez_montero/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.pmddmm_eval1_javier_rodriguez_montero;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.pmddmm_eval1_javier_rodriguez_montero";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
